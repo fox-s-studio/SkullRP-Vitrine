6778cbaff0302769c09d4b8e2c88ee650ebf81b5
Skullyfox <Skullyfox.yt@gmail.com>
2019-08-04 18:29:05 +0200

    head embed code

--- src/Component/head.jsx
+++ src/Component/head.jsx
@@ -8,10 +8,10 @@ function Head(props) {
         <link rel="shortcut icon" href={props.Logo} />
         <meta name="twitter:title" content="[FR] Skull RP - Serveur GTA V RP" />
         <meta name="twitter:description" content="Serveur GTA V RP Français, rejoins nous !" />
-        <meta name="twitter:image" content={props.Preview} />
+        <meta name="twitter:image" content={'http://skull-rp.foxstud.io/'+props.Preview} />
         <meta property="og:title" content="[FR] Skull RP - Serveur GTA V RP" />
         <meta property="og:description" content="Serveur GTA V RP Français, rejoins nous !" />
-        <meta property="og:image" content={props.Preview} />
+        <meta property="og:image" content={'http://skull-rp.foxstud.io/'+props.Preview} />
         <meta property="og:image:width" content="1218" />
         <meta property="og:image:height" content="608" />
         <meta name="twitter:card" content="summary_large_image" />
