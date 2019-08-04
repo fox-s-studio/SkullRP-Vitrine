c0e49284dcac5c1d454e4b5d279788d3faba8d1c
Skullyfox <Skullyfox.yt@gmail.com>
2019-08-04 18:00:47 +0200

    head embed code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Header from './Component/header';
 import Logo from './img/logo.png';
-import Helmet from 'react-helmet';
+import Preview from './img/preview.png';
+import Head from './Component/head';
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <title>[FR] Skull RP</title>
-        <link rel="shortcut icon" href={Logo} />
-      </Helmet>
+      <Head Logo={Logo} Preview={Preview} />
       <Header logo={Logo} />
     </div>
   );
---
+++ src/Component/head.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+function Head(props) {
+    return(
+      <Helmet>
+        <title>[FR] Skull RP</title>
+        <link rel="shortcut icon" href={props.Logo} />
+        <meta name="twitter:title" content="[FR] Skull RP - Serveur GTA V RP" />
+        <meta name="twitter:description" content="Serveur GTA V RP Français, rejoins nous !" />
+        <meta name="twitter:image" content={props.Preview} />
+        <meta property="og:title" content="[FR] Skull RP - Serveur GTA V RP" />
+        <meta property="og:description" content="Serveur GTA V RP Français, rejoins nous !" />
+        <meta property="og:image" content={props.Preview} />
+        <meta property="og:image:width" content="1218" />
+        <meta property="og:image:height" content="608" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image:type" content="image/png" />
+    </Helmet>
+    )
+};
+
+export default Head;
