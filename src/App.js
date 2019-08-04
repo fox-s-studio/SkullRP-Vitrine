905c74c57dac03d91d03abbcf8dc033513eb43f4
Skullyfox <Skullyfox.yt@gmail.com>
2019-08-04 18:39:18 +0200

    head embed code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from './Component/header';
 import Logo from './img/logo.png';
-import Head from './Component/head';
 
 function App() {
   return (
     <div className="App">
-      <Head/>
       <Header logo={Logo} />
     </div>
   );
