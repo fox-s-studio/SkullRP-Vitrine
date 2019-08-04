fe63ab1fe91ba741ec1039fc3ab2d80ffe57b524
Skullyfox <Skullyfox.yt@gmail.com>
2019-08-04 17:42:55 +0200

    Discord button added

--- src/Component/header.jsx
+++ src/Component/header.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Discord } from 'styled-icons/boxicons-logos/Discord';
+
+const WhiteDiscord = styled(Discord)`
+  color: #c8d6e5;
+`
 
 function Header(props){
     return(
@@ -8,7 +14,17 @@ function Header(props){
                     <img src={props.logo} alt="skullrp-logo"/>
                 </div>
                 <div className="Title">
-                    Bienvenue sur SkullRP !
+                    <p>Bienvenue sur SkullRP !</p>
+                </div>
+                <div className="JoinContainer">
+                    <div className="discord">
+                        <a 
+                            href="https://discord.gg/Ea4rEZQ" 
+                            target="_blank" 
+                            rel="noopener noreferrer"> </a>
+                        <WhiteDiscord />
+                        <p>Rejoignez notre Discord</p>
+                    </div>
                 </div>
             </div>
         </div>
