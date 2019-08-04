f2441bd6f9af5b41006263110dfd74f7922b81cd
Skullyfox <Skullyfox.yt@gmail.com>
2019-08-04 15:43:59 +0200

    soft changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './Component/header';
 import Logo from './img/logo.png';
+import Helmet from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>[FR] Skull RP</title>
+        <link rel="shortcut icon" href={Logo} />
+      </Helmet>
       <Header logo={Logo} />
     </div>
   );
